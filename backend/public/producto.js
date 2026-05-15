88bec03a1c9b79b0725d9cc4735f11937865f0c9
Avance <nixonandres_96@hotmail.com>
2026-05-14 19:53:15 -0500

    coore

--- backend/public/producto.js
+++ backend/public/producto.js
@@ -52,8 +52,7 @@ function mostrarproducto(producto){
         
         // enlace para imagenes de productos
         div.innerHTML=`
-        <img src="${p.imagen}" alt="${p.nombre}"> 
-        <p>$${p.imagen}</p>
+        <img src="${p.imagen}" alt="${p.nombre}">
         <h4>${p.nombre}</h4>
         <p>$${p.precio}</p>
         <div class="contador">
